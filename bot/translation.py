79f14bbee852bd90a9b0f6959d9b2d72c5bbb269
universitywits1 <85569369+universitywits1@users.noreply.github.com>
2021-08-02 17:54:57 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -16,11 +16,11 @@ class Translation(object):
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/genaral07">༒༈ĐΔ€ ŞỮ ØĦ༈༒</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/genaral7">༒༈ĐΔ€ ŞỮ ØĦ༈༒</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://t.me/Movies360help/68">Click Me</a></i>
 """
